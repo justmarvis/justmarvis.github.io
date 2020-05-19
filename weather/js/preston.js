885a2f654332c13446bf3ffa2fe7a03d8a1ea8d7
jusmemarvis <marvinomeccozi@gmail.com>
2020-05-19 02:42:58 -0600

    Added content

--- weather/js/preston.js
+++ weather/js/preston.js
@@ -1,6 +1,5 @@
 
-  $(document).ready(function(){
-    $('menu-toggle').click(function(){
-      $('nav').toggleClass('.active')
-    })
-  })
+  const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
