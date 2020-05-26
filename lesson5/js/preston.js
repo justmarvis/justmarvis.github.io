70ffa1942e3880ff7144db1406c6110eee2a12da
jusmemarvis <marvinomeccozi@gmail.com>
2020-05-26 07:23:59 -0600

    Added contents, edited page and added designs

--- lesson5/js/preston.js
+++ lesson5/js/preston.js
@@ -3,3 +3,20 @@
 const mainnav = document.querySelector('.navigation')
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+  function park() {
+    var mydate = new Date();
+
+    var day = mydate.getDay();
+    var month = mydate.getMonth();
+    var daym = mydate.getDate();
+    var dayarray = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+
+    var park = document.getElementById("parkBanner").value
+    
+      if (daym = "Saturday") {
+        var parkDisplay = print("park");
+      }
+
+      document.getElementById("parkBanner").innerHTMl = parkDisplay;
+  }
