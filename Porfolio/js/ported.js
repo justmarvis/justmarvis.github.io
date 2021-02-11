44c693e3c4789960e5ea7162bcdc4ca506233b96
jusmemarvis <marvinomeccozi@gmail.com>
2021-02-11 13:46:27 -0700

    reworked design

---
+++ Porfolio/js/ported.js
@@ -0,0 +1,13 @@
+
+
+// footer
+  let week_names = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thurday', 'Friday', 'Saturday'];
+let month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+    'November', 'December'];
+let date = new Date();
+let weekname = week_names[date.getDay()];
+let monthname = month_names[date.getMonth()];
+
+const dateoutput = document.querySelector('#currentdate');
+
+dateoutput.textContent = "Last Updated: " + weekname + ", " + date.getDate() + " " + monthname + " " + date.getFullYear();
