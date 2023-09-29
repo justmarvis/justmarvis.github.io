4261ec36e29dc5055e7da1d8deb33bac2ead5edb
Most Often Delicate <marvisdev98@gmail.com>
2023-09-29 14:28:09 -0400

    updated

--- assets/js/ported.js
+++ assets/js/ported.js
@@ -72,12 +72,12 @@ function myComment() {
 
 // footer
   let week_names = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-let month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
-    'November', 'December'];
+let month_names = ['Jan', 'Feb', 'Mar', 'Aprl', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct',
+    'Nov', 'Dec'];
 let date = new Date();
 let weekname = week_names[date.getDay()];
 let monthname = month_names[date.getMonth()];
 
 const dateoutput = document.querySelector('#currentdate');
 
-dateoutput.textContent = "Updated: " + weekname + ", " + date.getDate() + " " + monthname + " " + date.getFullYear();
+dateoutput.textContent = date.getDate() + " " + monthname + " " + date.getFullYear();
