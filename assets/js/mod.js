8033e44f2e5d04bdde3d0508487d59e63fad3f1d
Most Often Delicate <marvisdev98@gmail.com>
2024-04-22 21:40:10 -0400

    updated

--- assets/js/mod.js
+++ assets/js/mod.js
@@ -13,6 +13,16 @@ function type() {
 
 type();
 
+// Function to check if an element is in the viewport
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
 
 // Function to check if an element is in the viewport
 function isInViewport(element) {
