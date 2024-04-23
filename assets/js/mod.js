62fde390f52c292677bf9fb15d994d87c58348c8
Most Often Delicate <marvisdev98@gmail.com>
2024-04-22 23:07:08 -0400

    updated

--- assets/js/mod.js
+++ assets/js/mod.js
@@ -24,17 +24,6 @@ function isElementInViewport(el) {
   );
 }
 
-// Function to check if an element is in the viewport
-function isInViewport(element) {
-  const rect = element.getBoundingClientRect();
-  return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
-
 // Function to add animation class when element is in view
 function handleIntersection(entries, observer) {
   entries.forEach(entry => {
@@ -49,4 +38,12 @@ function handleIntersection(entries, observer) {
 const observer = new IntersectionObserver(handleIntersection, { threshold: 0 });
 
 // Observe the target element
-observer.observe(document.getElementsByClassName('loadElement'));
+observer.observe(document.getElementById('loadElement'));
+observer.observe(document.getElementById('loadElement1'));
+observer.observe(document.getElementById('loadElement2'));
+observer.observe(document.getElementById('loadElement3'));
+observer.observe(document.getElementById('loadElement4'));
+observer.observe(document.getElementById('loadElement5'));
+observer.observe(document.getElementById('loadElement6'));
+observer.observe(document.getElementById('loadElement7'));
+observer.observe(document.getElementById('loadElement8'));
