6e7b2379bd71e721024f6f946a381f530430f9f6
Justmarvis <marvinomeccozi@gmail.com>
2024-10-14 00:35:25 -0400

    updated

--- assets/js/marvis.js
+++ assets/js/marvis.js
@@ -21,6 +21,44 @@ function fas() {
 function mig() {
   window.open('https://www.figma.com/design/78TK3YYOU1UfNZifO87bBr/MIG?node-id=0-1&t=SuYg793DvfWtNZfO-1', '_blank');
 }
+function res() {
+  window.open('https://cdn.glitch.global/20f7db08-2c65-491e-87a0-f660f15456ba/Marvis%20Igbineweka%20Resume%20_%20CV%20(1).pdf?v=1728879424490', '_blank');
+}
+function msg() {
+  window.open('https://www.linkedin.com/in/marvis-igbineweka?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app', '_blank');
+}
+function cert() {
+  const certWindow = window.open('', '_blank', 'width=800,height=600');
+  certWindow.document.write(`
+    <html>
+      <head>
+        <title>Certificate Preview</title>
+        <style>
+          body {
+            margin: 0;
+            overflow: hidden;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #000;
+          }
+          img {
+            max-width: 100%;
+            max-height: 100%;
+            pointer-events: none; /* Disable interactions like right-click */
+            user-select: none; /* Prevent text/image selection */
+          }
+        </style>
+      </head>
+      <body>
+        <img src="https://cdn.glitch.global/20f7db08-2c65-491e-87a0-f660f15456ba/UI_UX%20Design%20Expert%20Master%20Certificate.jpg?v=1728869615586" 
+             alt="Certificate" />
+      </body>
+    </html>
+  `);
+  certWindow.document.close(); // Ensures the document is properly rendered.
+}
+
 
 //jump to section through links
 function services() {
